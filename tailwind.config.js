ae5ab5fe40d90e6a491f4747dae91092ddc27ba0
Askaa10 <149747430+Askaa10@users.noreply.github.com>
2024-03-25 09:39:22 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+      },
+      f2:"#477577",
+      f1:"#70a2ff",
+      backgroundImage: {
+      }
+    },
+  },
+  plugins: [],
+  darkMode:'class'
+}
